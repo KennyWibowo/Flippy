142ad13e17b40fe81613eb888b4cd1dc634764ab
AbishekGollapudi <agollapudi@ucdavis.edu>
2016-04-30 16:52:12 -0700

    changed stuff

--- app/src/main/java/com/helloworld/kenny/flippy/MainActivity.java
+++ app/src/main/java/com/helloworld/kenny/flippy/MainActivity.java
@@ -1,15 +1,27 @@
 package com.helloworld.kenny.flippy;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mySound;
     private int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySound = MediaPlayer.create(this, R.raw.memories);
+        mySound.setLooping(true);
+        mySound.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mySound.release();
+        finish();
     }
 }
