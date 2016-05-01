fb8ef985c7094c84fbdcf9f92ddd27ace9ab34ba
Michelle Tang <Michelle Tang>
2016-04-30 17:29:06 -0700

    initial instructions

--- app/src/main/java/com/helloworld/kenny/flippy/MainActivity.java
+++ app/src/main/java/com/helloworld/kenny/flippy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.helloworld.kenny.flippy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void giveIntformation(View v)
+    {
+        String button_text;
+        button_text =((Button) v).getText().toString();
+        if(button_text.equals("How to Play"))
+        {
+            Intent intent = new Intent(this,Instructions.class);
+            startActivity(intent);
+
+
+        }
+    }
+
 }
